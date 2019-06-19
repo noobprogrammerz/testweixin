ebf23bc3496499d62558f94abe1fde5f1fe5d197
noobprogrammerz <30626118+noobprogrammerz@users.noreply.github.com>
2019-06-20 03:17:30 +0800

    init project

---
+++ app/index/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\index\controller;
+use think\Controller;
+use think\image\Exception;
+
+class Index extends Controller
+{
+    public function index()
+    {
+    
+    }
+
+}
